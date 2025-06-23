fabc2b3b3b8ae7506baee3c4ae67fa85a46e1393
Alphi <elmolaygo.1@gmail.com>
2025-06-23 18:58:31 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var alphiButton = document.getElementById('alphiButton');
+var marckButton = document.getElementById('marckButton');
+var marcButton = document.getElementById('marcButton');
+
+var alphiModal = document.getElementById('alphiModal');
+var marckModal = document.getElementById('marckModal');
+var marcModal = document.getElementById('marcModal');
+var closeButton = document.getElementById('closeButton');
+
+document.addEventListener('DOMContentLoaded', function() {
+    alphiButton.addEventListener('click', function() {
+        alphiModal.classList.add('active');
+    })
+    marckButton.addEventListener('click', function() {
+        marckModal.classList.add('active');
+    })
+    marcButton.addEventListener('click', function() {
+        marcModal.classList.add('active');
+    })
+    closeButtonA.addEventListener('click', function() {
+        alphiModal.classList.remove('active');
+    })
+    closeButtonB.addEventListener('click', function() {
+        marckModal.classList.remove('active');
+    })
+    closeButtonC.addEventListener('click', function() {
+        marcModal.classList.remove('active');
+    })
+});
